c1927c325f9bd5eef083c637061a659c9e244ccb
Jeff Seymour <seym1585@gmail.com>
2021-04-21 21:41:56 -0700

    add templates

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
 const logger = require('./middleware/logger');
 const app = express();
-
+const members = require('./Members');
 
 
 // init middleware
 // app.use(logger);
 
+// Handlebars Middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 // Body Parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+// Homepage Route
+app.get('/', (req, res) => res.render('index', {
+    title: 'Member App',
+    members
+}));
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
