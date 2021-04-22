fa1307a2cfd9f4028fc48433a311b74a615a4861
Jeff Seymour <seym1585@gmail.com>
2021-04-21 21:10:15 -0700

    add get put post and delete routes for members api

--- routes/api/members.js
+++ routes/api/members.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const members = require('../../Members');
+const uuid = require('uuid');
+const e = require('express');
 
 // Gets all Members
 router.get('/', (req, res) => {
@@ -13,6 +15,55 @@ router.get('/:id', (req, res) => {
 
     if (found) {
         res.json(members.filter(member => member.id === parseInt(req.params.id)));
+    } else {
+        res.status(400).json({msg: `No member with id of ${req.params.id} found.`});
+    }
+});
+
+// Create Member
+router.post('/', (req, res) => {
+    const newMember = {
+        id: uuid.v4(),
+        name: req.body.name,
+        email: req.body.email,
+        status: 'active'
+    };
+
+    if (!newMember.name || !newMember.email) {
+        return res.status(400).json({msg: 'Please include a name and email'});
+    }
+
+    members.push(newMember);
+    res.json(members);
+});
+
+// Update Member
+router.put('/:id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id));
+
+    if (found) {
+        const updMember = req.body;
+        members.forEach((member) => {
+            if (member.id === parseInt(req.params.id)) {
+                member.name = updMember.name ? updMember.name : member.name;
+                member.email = updMember.email ? updMember.email : member.email;
+                res.json({msg: 'Member updated', member});
+            }
+        });
+    } else {
+        res.status(400).json({msg: `No member with id of ${req.params.id} found.`});
+    }
+});
+
+// Delete Member
+router.delete('/:id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id));
+
+    if (found) {
+        res.json({
+            msg: 'Member deleted', 
+            members: members.filter(member => member.id !== parseInt(req.params.id))
+        });
     } else {
         res.status(400).json({msg: `No member with id of ${req.params.id} found.`});
     }
